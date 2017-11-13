a30f3ea5cdcec5f45e038f7d142fa0fb081bec5a
Naomi Mariga <kaburamariga@gmail.com>
2017-11-14 00:05:14 +0300

    complete version 1 features

--- user.py
+++ user.py
@@ -2,13 +2,14 @@ import random
 from sqlalchemy.sql import text
 from validate_email import validate_email
 from utilities import Utilities
+import smtplib
 
 
 class User(Utilities):
     def check_email_exists(self, email):
         try:
             sql = "SELECT COUNT(email) AS emails FROM users WHERE email= :email"
-            connection, metadata = self.database_connection()
+            connection = self.database_connection()
             stmt = text(sql)
             stmt = stmt.bindparams(email=email)
             result = connection.execute(stmt)
@@ -32,8 +33,8 @@ class User(Utilities):
                                 if any(letter.isupper()for letter in password) and any(letter.islower() for letter in password) \
                                    and any(letter.isdigit() for letter in password):
 
-                                    sql = "INSERT INTO users (email, username, pword) VALUES (:email, :username :pword)"
-                                    connection, metadata = self.database_connection()
+                                    sql = "INSERT INTO users (email, username, pword) VALUES (:email, :username, :pword)"
+                                    connection = self.database_connection()
                                     stmt = text(sql)
                                     stmt = stmt.bindparams(email=email, username=username, pword=password)
                                     connection.execute(stmt)
@@ -45,7 +46,8 @@ class User(Utilities):
                                     message = "Password must contain uppercase, lowercase and digits"
                             else:
                                 message = "password must be 6 or more characters in length"
-                        message = "password contains only letters and numbers and it should be not null or an empty string"
+                        else:
+                            message = "password contains only letters and numbers and it should be not null or an empty string"
                     else:
                         message = "username contains only letters and numbers, at least 3 characters long and it can not be empty"
                 else:
@@ -69,9 +71,9 @@ class User(Utilities):
         try:
             if validate_email(email):
                 if self.check_email_exists(email):
-                    if password:
-                        sql = "SELECT email, username, user_id, pword FROM users WHERE email == :email AND pword ==:pword"
-                        connection, metadata = self.database_connection()
+                    if password.strip():
+                        sql = "SELECT email, username, user_id, pword FROM users WHERE email = :email AND pword =:pword"
+                        connection = self.database_connection()
                         stmt = text(sql)
                         stmt = stmt.bindparams(email=email, pword=password)
                         result = connection.execute(stmt)
@@ -83,8 +85,8 @@ class User(Utilities):
                             auth_token = self.auth_token("abcdefghijklmnopqrstuvwzyz1234567890", 20)
                             message = {
                                 "message": "Login was successful, Welcome" + row["username"],
-                                "user_id" : row["user_id"],
-                                "token": auth_token
+                                "user_id": row["user_id"],
+                                "auth_token": auth_token
                             }
                             sql = "INSERT INTO authentication (user_id, auth_token) VALUES (:user_id, :auth_token)"
                             stmt = text(sql)
@@ -93,7 +95,7 @@ class User(Utilities):
                         else:
                             message = "No match was found"
                     else:
-                        message = "provide password"
+                        message = "password has spaces"
                 else:
                     message = "Email is not registered with us"
             else:
@@ -106,17 +108,155 @@ class User(Utilities):
             "message": message
         }
 
-    def reset_password(self, email):
-        return
+    def send_email(self, receiver, subject, message):
+        try:
+            smtp = smtplib.SMTP("smtp.gmail.com", 587)  # initialize smtp class
+            smtp.ehlo()
+            smtp.starttls()
+            smtp.ehlo()
+            smtp.login("prettynoms@gmail.com", "shoppinglist1")
+            header = 'To:' + receiver + '\nFrom: shopping list<prettynoms@gmail.com>\nSubject: ' + subject + '\n'
+            msg = header + '\n' + message + '\n\n'
+            smtp.sendmail("prettynoms@gmail.com", receiver, msg)
+            smtp.close()
+            return True
+        except Exception:
+            return False
 
-    def token(self):
-        return
+    def reset_password(self, email):
+        success = False
+        try:
+            if validate_email(email):
+                if self.check_email_exists(email):
+                    sql = "SELECT email, user_id, pword, username FROM users WHERE email = :email"
+                    connection = self.database_connection()
+                    stmt = text(sql)
+                    stmt = stmt.bindparams(email=email)
+                    result = connection.execute(stmt)
+                    row = result.fetchone()
+
+                    if row is not None:
+                        email = row["email"]
+                        user_id = row["user_id"]
+                        print(user_id)
+                        username = row["username"]
+                        temp_password = self.auth_token("abcdefghijklmnopqrstuvwxyz", 10)
+                        print(temp_password)
+                        message_body = " Hi " + username + "\n Please use this temporary password to login in \n" + temp_password
+                        if self.send_email(email, "Shoppinglist Password Reset", message_body):
+                            sql = "UPDATE users SET pword = :temp_password WHERE user_id = :user_id "
+                            stmt = text(sql)
+                            stmt = stmt.bindparams(user_id=user_id, temp_password=temp_password)
+                            print(temp_password)
+                            connection = self.database_connection()
+                            connection.execute(stmt)
+                            success = True
+                            message = "password was changed"
+                        else:
+                            message = "Error sending password reset email"
+                    else:
+                        message = "No match was found"
+                else:
+                    message = "email does not exist in our database"
+            else:
+                message = "invalid email format detected"
+        except Exception as error:
+            message = "An exception error occurred " + str(error)
+        return {
+            "success": success,
+            "message": message
+        }
 
     def user_is_logged_in(self, user_id, token):
-        return
+        success = False
+        try:
+            sql = "SELECT COUNT(user_id) as cols FROM authentication WHERE user_id = :user_id AND auth_token = :auth_token"
+            connection = self.database_connection()
+            stmt = text(sql)
+            stmt = stmt.bindparams(user_id=user_id, auth_token=token)
+            result = connection.execute(stmt)
+            row = result.fetchone()
+            if row is not None:
+                rows = row["cols"]
+                if int(rows) > 0:
+                    success = True
+                    message = "user is logged in"
+                else:
+                    message = "User is not logged in"
+            else:
+                message = "User is not logged in"
+        except Exception as error:
+            message = "An exception error occurred" + str(error)
+        return{
+            "success": success,
+            "message": message
+        }
 
     def change_password(self, user_id, token, old_password, new_password):
-        return
+        success = False
+        try:
+            logged_in = self.user_is_logged_in(user_id, token)
+            if logged_in["success"]:
+                sql = "SELECT pword FROM users WHERE user_id = :user_id"
+                connection = self.database_connection()
+                stmt = text(sql)
+                stmt = stmt.bindparams(user_id=user_id)
+                result = connection.execute(stmt)
+                row = result.fetchone()
+
+                if row is not None:
+                    if row["pword"] == old_password:
+                        if new_password is not None and new_password.isalnum and new_password.strip is not "":
+                            if any(letter.isdigit() for letter in new_password) and any(letter.islower() for letter in new_password) \
+                               and any(letter.isupper() for letter in new_password):
+                                if len(new_password) >= 6:
+                                    sql = "UPDATE users SET pword = :pword WHERE user_id = :user_id"
+                                    stmt = text(sql)
+                                    stmt = stmt.bindparams(pword=new_password, user_id=user_id)
+                                    connection.execute(stmt)
+                                    success = True
+                                    message = "password Changed successfully"
+                                else:
+                                    message = "password length should be at least 6 characters"
+                            else:
+                                message = "use mixed uppercase lowercase and numbers for a strong password"
+                        else:
+                            message = "password only contains letters and numbers and cannot be null or an empty string"
+                    else:
+                        message = "passwords do not match"
+                else:
+                    message = "No match found"
+            else:
+                message = "user must be logged in to perform this action"
+        except Exception as error:
+            message = "An exception error occurred" + str(error)
+        return{
+            "success": success,
+            "message": message
+        }
 
     def log_out(self, user_id, token):
-        return
+        success = False
+        try:
+            logged_in = self.user_is_logged_in(user_id, token)
+            if logged_in["success"]:
+                sql = "DELETE FROM authentication WHERE user_id = :user_id AND auth_token = :auth_token"
+                connection = self.database_connection()
+                stmt = text(sql)
+                stmt = stmt.bindparams(user_id=user_id, auth_token=token)
+                result = connection.execute(stmt)
+                row = result.fetchone
+
+                if row is not None:
+                    success = True
+                    message = "User successfully logged out"
+                else:
+                    message = "No match found, user is not logged in"
+            else:
+                message = "user must be logged in to perform this action"
+        except Exception as error:
+            message = "An exception error occurred " + str(error)
+        return{
+            "success": success,
+            "message": message
+        }
