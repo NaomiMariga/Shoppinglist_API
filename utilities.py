d50c8c8ca752753ba0e238eb05175f65f3e40fbe
Naomi Mariga <kaburamariga@gmail.com>
2017-11-14 00:06:11 +0300

    remove metadata method

--- utilities.py
+++ utilities.py
@@ -6,13 +6,15 @@ import sqlalchemy
 
 class Utilities:
     def database_connection(self):
-        url = "postgresql://postgres:Theology@localhost:5432/shoppinglist"
+        try:
+            url = "postgresql://postgres:Theology@localhost:5432/shoppinglist"
 
-        connection = sqlalchemy.create_engine(url)
+            connection = sqlalchemy.create_engine(url)
 
-        # binds connection to the metadata
-        metadata = sqlalchemy.MetaData(bind=connection, reflect=True)
+            "returns a connection"
+        except Exception as error:
+            connection = None
+            print(str(error))
 
-        "returns a connection and metadata"
-        return connection, metadata
+        return connection
 
