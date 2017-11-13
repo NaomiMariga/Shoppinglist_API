322ee370c0fd38c19fb4f4fb3b9a5d58a06c4d40
Naomi Mariga <kaburamariga@gmail.com>
2017-11-13 15:13:55 +0300

    Add shopping list class defined functions

---
+++ shoppinglist.py
@@ -0,0 +1,25 @@
+class Shoppinglist:
+
+    def create_shoppinglist(self, user_id, token, list_name):
+        return
+
+    def read_shoppinglist(self, user_id, token):
+        return
+
+    def edit_shoppinglist(self, user_id, token, list_id, list_name):
+        return
+
+    def delete_shoppinglist(self, user_id, token, list_id):
+        return
+
+    def add_items(self, user_id, token, list_id, item_name, quantity, units, cost):
+        return
+
+    def read_items(self, user_id, token, list_id):
+        return
+
+    def edit_items(self, user_id, token, list_id, item_id, attribute, value):
+        return
+
+    def delete_items(self, user_id, token, list_id, item_id):
+        return
