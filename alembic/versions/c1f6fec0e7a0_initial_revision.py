0539669e2f93b17117334061a3c7b0e987ffe259
Naomi Mariga <kaburamariga@gmail.com>
2017-11-21 21:04:11 +0300

    Add functional database migration

--- alembic/versions/c1f6fec0e7a0_initial_revision.py
+++ alembic/versions/c1f6fec0e7a0_initial_revision.py
@@ -20,29 +20,28 @@ def upgrade():
     op.create_table(
         'users',
         sa.Column('user_id', sa.BigInteger, primary_key=True),
-        sa.column('email', sa.String(100), nullable=False),
-        sa.Column('username', sa.String(50), nullable=False),
-        sa.Column('pword', sa.String(64), nullable=False),
-        sa.column('date_registered', sa.TIMESTAMP, default=sa.func.now())
-
+        sa.Column('email', sa.String(100)),
+        sa.Column('username', sa.String(50)),
+        sa.Column('pword', sa.String(64)),
+        sa.Column('date_registered', sa.DateTime)
     )
     op.create_table(
         'lists',
         sa.Column('list_id', sa.BigInteger, primary_key=True),
         sa.Column('user_id', sa.Integer),
-        sa.Column('list_name', sa.String(50), nullable=False),
-        sa.Column('time_updated', sa.TIMESTAMP, default=sa.func.now())
+        sa.Column('list_name', sa.String(50)),
+        sa.Column('time_updated', sa.DateTime, server_default=sa.func.current_timestamp())
     )
 
     op.create_table(
         'items',
         sa.Column('item_id', sa.BigInteger, primary_key=True),
         sa.Column('list_id', sa.Integer),
-        sa.Column('item_name', sa.String(50), nullable=False),
+        sa.Column('item_name', sa.String(50)),
         sa.Column('quantity', sa.Float),
         sa.Column('units', sa.String(5)),
         sa.Column('item_cost', sa.Float),
-        sa.Column('time_updated', sa.TIMESTAMP, default=sa.func.now())
+        sa.Column('time_updated', sa.DateTime, server_default=sa.func.current_timestamp())
 
     )
     op.create_table(
@@ -50,25 +49,31 @@ def upgrade():
         sa.Column('auth_id', sa.BigInteger, primary_key=True),
         sa.Column('user_id', sa.Integer),
         sa.Column('auth_token', sa.String(100)),
-        sa.Column('login_time', sa.TIMESTAMP, default=sa.func.now())
+        sa.Column('login_time', sa.DateTime, server_default=sa.func.current_timestamp())
 
     )
     op.create_foreign_key(
-        'user_id',
-        'users', 'authentication',
+        'fk_lists_user_id',
+        'lists', 'users',
+        ['user_id'], ['user_id'],
+    )
+    op.create_foreign_key(
+        'fk_authentication_user_id',
+        'authentication', 'users',
         ['user_id'], ['user_id']
     )
     op.create_foreign_key(
-        'list_id',
-        'lists',
-        ['list_id']
+        'fk_items_list_id',
+        'items', 'lists',
+        ['list_id'], ['list_id'],
     )
 
 
 def downgrade():
-    op.drop_table('users')
-    op.drop_table('lists')
-    op.drop_table('items')
     op.drop_table('authentication')
-    op.drop_foreign_key('user_id')
-    op.drop_foreign_key('list_id')
+    op.drop_table('items')
+    op.drop_table('lists')
+    op.drop_table('users')
+
+
+
