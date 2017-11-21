f39e523c4ab39f490d2ebf90e920f90a98f00115
Naomi Mariga <kaburamariga@gmail.com>
2017-11-21 21:41:18 +0300

    add heroku database

--- utilities.py
+++ utilities.py
@@ -1,13 +1,14 @@
 """
 Defines the frequently used functions
 """
+import os
 import sqlalchemy
 
 
 class Utilities:
     def database_connection(self):
         try:
-            url = "postgresql://shoppinglist:Andela100@localhost:5432/shoppinglist"
+            url = os.environ.get('DATABASE_URL', "postgresql://shoppinglist:Andela100@localhost:5432/shoppinglist")
 
             connection = sqlalchemy.create_engine(url)
 
